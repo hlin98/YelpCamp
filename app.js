27790c4a889918f160c7a0cacbfe24f22f5b5148
Helen Lin <30956318+hlin98@users.noreply.github.com>
2018-07-19 10:22:47 -0400

    remove geocoder

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var express         = require("express"),
     Comment         = require("./models/comment"),
     User            = require("./models/user"),
     seedDB          = require("./seeds")
-    geocoder        = require('geocoder');
 
 // requiring routes
 var commentRoutes = require("./routes/comments"),
